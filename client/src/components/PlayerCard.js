da39d1e4ea775d34ccc632fce4be67bb0ae346dd
Dennis Padiernos <depadiernos@gmail.com>
2019-10-11 20:15:15 -0500

    implement playercard tests

--- client/src/components/PlayerCard.js
+++ client/src/components/PlayerCard.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function PlayerList({ player }) {
   return (
     <div>
-      <h1>{player.name}</h1>
+      <h1>Name: {player.name}</h1>
       <h2>{player.country}</h2>
       <h2>{player.searches}</h2>
     </div>
--- client/src/components/__tests__/PlayerCard.test.js
+++ client/src/components/__tests__/PlayerCard.test.js
@@ -11,26 +11,21 @@ const mockData = {
   searches: 100
 };
 
-test("Render Ball Count", () => {
-  const wrapper = rtl.render(<Display data={mockData} />);
-  const element = wrapper.getByText(/ball/i);
-  expect(element).toHaveTextContent("Ball: 3");
+test("shows name", () => {
+  const wrapper = rtl.render(<PlayerCard player={mockData} />);
+  const element = wrapper.getByText(/name:/iw);
+  expect(element).toHaveTextContent(/alex morgan/i);
 });
 
-test("Render Strike Count", () => {
-  const wrapper = rtl.render(<Display data={mockData} />);
-  const element = wrapper.getByText(/strike/i);
-  expect(element).toHaveTextContent("Strike: 2");
+test("shows country", () => {
+  const wrapper = rtl.render(<PlayerCard player={mockData} />);
+  const element = wrapper.getByText(mockData.country);
+  expect(element).toHaveTextContent("United States");
 });
 
-test("Render Foul Count", () => {
-  const wrapper = rtl.render(<Display data={mockData} />);
-  const element = wrapper.getByText(/foul/i);
-  expect(element).toHaveTextContent("Foul: 4");
-});
 
-test("Render Hit Count", () => {
-  const wrapper = rtl.render(<Display data={mockData} />);
-  const element = wrapper.getByText(/hit/i);
-  expect(element).toHaveTextContent("Hit: 10");
+test("shows searches", () => {
+  const wrapper = rtl.render(<PlayerCard player={mockData} />);
+  const element = wrapper.getByText(mockData.searches.toString());
+  expect(element).toHaveTextContent("100");
 });
